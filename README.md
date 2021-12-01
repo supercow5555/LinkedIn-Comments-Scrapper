bb56e6da10397680abbdaf8f74876e64d3995cfb
supercow5555 <kkaushal2001@gmail.com>
2021-12-02 01:26:59 +0800

    working v1.0
    working smoothly most of the time

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # LinkedIn Scrapper
 
-First try
+How to use:
+1. Enter username, password for linked 
+2. Enter post url 
+
+
+### Performance
+This script has been run on 3 different posts, with 37, 44, and 205 comments resepectivly with mostly success.
+
+Sometimes crashes when it can't detect more comments, but usually works when you run the programme again... Will try to fix later
--- main.py
+++ main.py
@@ -1,15 +1,78 @@
 from time import sleep
 import selenium
 import csv
+import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-# Enter post url
-post_url = "https://www.linkedin.com/posts/dragonflyhk_ali-health-er-report-activity-6839475952322449408-Pcc_/"
 
-def load_all_comments(class_name):
+### Enter details here!
+
+# User name and password
+linkedin_username = "........."
+linkedin_password = "........."
+
+# Enter post url
+post_url = "https://www.linkedin.com/posts/dragonflyhk_ali-health-er-report-activity-6839475952322449408-Pcc_"              # Example post with 44 commments
+
+
+
+
+
+### helper functions
+
+# comments loading functions
+def load_all_comments():
+    try:
+        loadcomments = driver.find_element_by_class_name("comments-comments-list__load-more-comments-button")
+        print("Loading comments")
+        while True:
+            loadcomments.click()
+            print("Loading...")
+            sleep(5)
+            try:
+                loadcomments = driver.find_element_by_class_name("comments-comments-list__load-more-comments-button")
+            except:
+                print("All comments have been loaded")
+                break
+    except:
+        print("already loaded")
+
+# def email_extractor(comments):
+#     emails = []
+#     for comment in comments:
+#         email_match = re.findall(r".+\@.+\..+", str(comment))
+#         if email_match:
+#             emails.append(email_match)
+#         else:
+#             emails.append("-")
+#     return emails
+
+# email extractor
+def extract_emails(all_comments):
+    emails = []
+    for comment in all_comments:
+        email_match = re.findall(r"[\w\.-]+@[\w\.-]+\.\w+", str(comment))
+        if email_match:
+            emails.append(email_match)
+        else:
+            emails.append("-")
+    return emails
+
+# transfer to csv
+def transfer_csv():
+    for name, headline, email, comment in zip(all_names, all_headlines, all_emails, all_comments):
+        writer.writerow([name, headline, email, comment.encode("utf-8")])
+
+
+
+
+
+
+
+# Start of programme
 
 # Open a CSV
 writer = csv.writer(
@@ -21,6 +84,7 @@ writer = csv.writer(
 )
 writer.writerow(["Name", "Headline", "Email", "Comment"])
 
+
 # Initializing the web driver
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 
@@ -28,10 +92,10 @@ driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 driver.get("https://www.linkedin.com/")
 
 username = driver.find_element_by_name("session_key")
-username.send_keys("...........")
+username.send_keys(linkedin_username)
 
 password = driver.find_element_by_name("session_password")
-password.send_keys("...........")
+password.send_keys(linkedin_password)
 
 signin = driver.find_element_by_xpath("""//*[@id="main-content"]/section[1]/div/div/form/button""")
 signin.click()
@@ -41,11 +105,40 @@ driver.get(post_url)
 
 
 # Loading all commments
+load_all_comments()
 
 
-loadcomments = driver.find_element_by_class_name("comments-comments-list__load-more-comments-button")
-loadcomments.click()
+# Stripping all comments raw
+comments = driver.find_elements_by_class_name("comments-comment-item__main-content")
+
+all_comments = []
+for comment in comments:
+    result = comment.text.strip()
+    all_comments.append(result)
+
+# Extracting names
+names = driver.find_elements_by_class_name("comments-post-meta__name-text")
+all_names = []
+for name in names:
+    result = name.text.strip()
+    all_names.append(result)
+
+# Extracting headlines
+headlines = driver.find_elements_by_class_name("comments-post-meta__headline")
+all_headlines = []
+for headline in headlines:
+    result = headline.text.strip()
+    all_headlines.append(result)
+
+# # Extracting profile picture links
+# pictures = driver.find_elements_by_class_name()
+
+# extract from all comments
+all_emails = extract_emails(all_comments)
 
 
-while(True):
-    pass
+# transfer to csv
+transfer_csv()
+
+# while(True):
+#     pass
